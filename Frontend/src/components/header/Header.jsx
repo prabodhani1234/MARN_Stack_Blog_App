2ba25f6b664933140406cb512add714b7f171653
prabodhani1234 <83935882+prabodhani1234@users.noreply.github.com>
2022-07-12 11:26:53 +0530

    change Home screen main image

--- Frontend/src/components/header/Header.jsx
+++ Frontend/src/components/header/Header.jsx
@@ -4,12 +4,12 @@ export default function Header() {
   return (
     <div className="header">
       <div className="headerTitles">
-        <span className="headerTitleSm">React Native</span>
+        <span className="headerTitleSm">Travel & Tour</span>
         <span className="headerTitleLg">Blog</span>
       </div>
       <img
         className="headerImg"
-        src="https://images.pexels.com/photos/1090977/pexels-photo-1090977.jpeg?cs=srgb&dl=pexels-irina-iriser-1090977.jpg&fm=jpg"
+        src="https://images.pexels.com/photos/4992803/pexels-photo-4992803.jpeg?cs=srgb&dl=pexels-rachel-claire-4992803.jpg&fm=jpg"
         alt=""
       />
     </div>
