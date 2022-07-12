4f5e5a1f2226da57c5b9c6bc0fdcd193f2224e10
prabodhani1234 <83935882+prabodhani1234@users.noreply.github.com>
2022-07-12 10:14:47 +0530

    Add frontend template

--- Frontend/src/components/topbar/TopBar.jsx
+++ Frontend/src/components/topbar/TopBar.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { Context } from "../../context/Context";
 import "./topbar.css";
 
+
 export default function TopBar() {
   const { user, dispatch } = useContext(Context);
   const PF = "http://localhost:5000/images/"
