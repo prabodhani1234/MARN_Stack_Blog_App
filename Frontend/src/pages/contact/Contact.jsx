d773911de25e66133b7011cc74aeeed6a0ef0079
prabodhani1234 <83935882+prabodhani1234@users.noreply.github.com>
2022-07-18 02:02:42 +0530

    design contact screen

--- Frontend/src/pages/contact/Contact.jsx
+++ Frontend/src/pages/contact/Contact.jsx
@@ -1,5 +1,46 @@
-export default function Contact(){
+import "./contact.css";
+
+export default function Contact() {
     return(
-        <div>cdsfsdfsd</div>
-    );
+        <div>
+            <div className="contact_title">
+                <h1 >Contact Us</h1>
+                <p style={{marginTop: "5px", fontSize:"17px", color: "rgb(66, 66, 66)"}}>Any question or remarks? Jest write us a message!</p>
+            </div>
+        <div className="contact_container">  
+            <div className="all_contact_container">
+                <div className="contact_left">
+                    <div className="contact_left_text">
+                       
+                    </div>
+                </div>
+                <div className="contact_right">
+                    <form className="contact_form_container">
+                        <label className="lable1">Name</label>
+						<input
+							type="text"
+                            className="contact_input"
+                            placeholder="Name"
+                        />
+                        <label className="lable2">Email</label>
+                        <input
+							type="text"
+                            className="contact_input"
+                            placeholder="Email"
+						/>
+                        <label className="lable3">What can we help you with? </label>
+						<textarea
+							type="text"
+                            className="contact_input_message"
+                            placeholder="Message"
+                        />
+                        <button type="submit" className="contact_btn">
+							Submit
+					    </button>  
+					</form>
+                </div>    
+            </div>
+        </div>
+        </div>
+    )
 }
