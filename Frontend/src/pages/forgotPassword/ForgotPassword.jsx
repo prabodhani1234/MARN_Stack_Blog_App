bbff2e990e78ab76c157dcde8a64ef3352e8c773
prabodhani1234 <83935882+prabodhani1234@users.noreply.github.com>
2022-07-17 12:07:37 +0530

    create contain of forget password screen

--- Frontend/src/pages/forgotPassword/ForgotPassword.jsx
+++ Frontend/src/pages/forgotPassword/ForgotPassword.jsx
@@ -1,7 +1,33 @@
 export default function ForgotPassword() {
     return(
-        <div>
-            
+        <div className="forgot_container">
+            <div className="all_forgot_container">
+                <div className="forgot_left">
+                    <div className="forgot_right_text">
+
+                    </div>
+                </div>
+                <div className="forgot_right">
+                    <h1 className="forgot-main_text">Forgot Password</h1> 
+                    <div>
+                        <p className="text1">Did someone forget this password</p>
+                        <p className="text2">That's Ok...</p>
+                        <p className="text3">Just enter the email address you've used to 
+                            <br/>register with use and we'll send you a rest link
+                        </p>
+                    </div>
+                    <form className="forgot_form_container">
+						<input
+							type="text"
+                            className="forgot_input"
+                            placeholder="Enter your email..."
+                        />
+                        <button type="submit" className="forgot_btn">
+							Sing Up
+					    </button>  
+					</form>
+                </div>    
+            </div>
         </div>
     )
 }
